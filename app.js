320d06150d0b18419810df8924e23d98b89dc584
Ayisha <alli.ayisha1@gmail.com>
2021-03-07 19:21:14 +0000

    inital commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//Variables 
+
+const acBtn = document.getElementById('ac-btn');
+const clearBtn = document.getElementById('clear-btn');
+const output = document.getElementById('output')
+
+//Numbers 
+const num9 = document.getElementById('num9');
+const num8 = document.getElementById('num8');
+const num7 = document.getElementById('num7');
+
+// Event Listeners
+
+acBtn.addEventListener('click', function(){
+    output.innerHTML = '0'
+})
+clearBtn.addEventListener('click', function(){
+    output.innerHTML = '0'
+})
+
+function displayButton (button){
+    output.innerHTML = button.innerHTML
+    
+}
+
