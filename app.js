d35d392e84ae4acb93d54d8634f7866813afbc6b
Ayisha Alli <alli.ayisha1@gmail.com>
2021-12-17 16:00:56 +0000

    restructured whole code

--- app.js
+++ app.js
@@ -1,102 +1,113 @@
 //Variables
 
-const acBtn = document.getElementById('ac-btn');
-const clearBtn = document.getElementById('clear-btn');
-const eachNumber = document.querySelectorAll('[data-number]');
-const keys = document.querySelector('.btns');
 
 
-//Numbers
-
-
+//Object holding all of our data
 const calculator = {
   'value': '0', //Sets default display value to 0 
   'firstOperand': null, // Set first Operand to Null 
   'waitingForSecondOpeerand': false, // Waits for second operand (Needs first and operator). Only then will it return true then the next input will be classed as the second Operand 
   'operator': 'null' // sets Operator to Null 
 } 
+
+//shorthand for writing the above object - Destructing 
 let {value, firstOperand, waitingForSecondOpeerand, operator} = calculator
 
-function displayValue (){
-  let output = document.getElementById('output');
-  output.innerHTML = value //value from Calculator object 
 
+function inputDigit(digit){
+  value === '0' ? value = digit : value = value + digit; // if display is 0, then adds number clicked, otherwise concats the number to existing number(s)
 }
 
-
-keys.addEventListener('click', function(e){
-  const {target} = e //gets target of click
-
-  //AC BUTTON 
-  if (target.classList.contains('ac')){
-    value = 0 
-    firstOperand = null
-    waitingForSecondOpeerand = false
-    operator = null
-    output.innerHTML = value
-  }
-
-  //CLEAR BUTTON TODO 
-  
-  if (target.classList.contains('op')){ //checks if operator was clicked 
-    console.log('Operator:', target.id) //shows what was clicked
-  }
-  
-  // INPUTTING NUMBER 
-  if (target.classList.contains('number')){ //checks if number was clicked 
-    console.log('number:', target.innerHTML) //shows what number was clicked 
-
-    if(firstOperand === null){ // if firstOperand is empty 
-      firstOperand = target.innerHTML // the innerHTML of target will become firstOperand
-      value = firstOperand
-      output.innerHTML = value
-      
-    } else if (firstOperand != null){ // every number pressed after will be added to the previous number 
-      value = value+target.innerHTML
-      console.log( firstOperand ) 
-      output.innerHTML = value
-
-    } 
-  }
-
-// TODDO : need to make sure that the other number after the operator is counted as the second operand 
-  if(target.classList.contains('op')){
-    if (target.id === 'add-btn'){
-      operator = '+' 
-      value = value + operator
-      output.innerHTML = value
-
-    } else if ( target.id === 'division-btn'){
-      operator.replace(operator, '/') 
-      value = value + operator
-      output.innerHTML = value
-    console.log(output.innerHTML)
-  }} 
-
-  //DECIMAL PLACE
-  if(target.id === 'decimal-btn'){
-    value = value + '.'
-    output.innerHTML = value
-
-  }
-  
-})
+function displayValue (){
+  const display = document.getElementById('output');
+  display.innerHTML = value //value from Calculator object 
+}
 
 displayValue()
 
-/*
-// Event Listeners
+// Event Listeners that checks which key was pressed
+ const keys = document.querySelector('.btns')
 
-acBtn.addEventListener('click', function () {
-  output.innerHTML = '0';
-});
-clearBtn.addEventListener('click', function () {
-  output.innerHTML = '0';
-});
+ keys.addEventListener('click', function(e){
+  const {target} = e //equiv to const target = event.target
+  
+  //check if operator was clicked 
+  if(target.classList.contains('op')){
+    console.log('operator:', target.id);
+    return;
+  }
 
-function displayButton(button) {
-  output.innerHTML = button.innerHTML;
-}
- */
+  // Check if number was clicked 
+   if(target.classList.contains('number')){
+     console.log('number:', target.innerHTML);
+     inputDigit(target.innerHTML)
+    }
+    //check if cancel btn was clicked 
+    if(target.classList.contains('cancel')){
+      console.log(target.id);
+      return;
+    }
+    //check if ac-btn was clicked 
+    if(target.classList.contains('ac')){
+      console.log(target.id);
+      return;
+    }
+    
+   
+
+   displayValue()
+ })
+
+
+
+
+
+// document.querySelector('.btns').addEventListener('click', function(e){
+//   const {target} = e //equiv to const target = event.target
+
+//   //AC BUTTON 
+//   if (target.classList.contains('ac')){
+//     value = '0' 
+//     firstOperand = null
+//     waitingForSecondOpeerand = false
+//     operator = null
+//     display.innerHTML = value
+//   }
+
+//   //CLEAR BUTTON TODO 
+  
+//   if (target.classList.contains('op')){ //checks if operator was clicked 
+//     console.log('Operator:', target.id) //shows what was clicked
+//     target.classList.add('isDepressed')
+//   }
+  
+//   // INPUTTING NUMBER 
+//   if(target.classList.contains('number')){
+//     console.log('number:', target.innerHTML)
+//     return;
+//   }
+  
+
+//     // TODDO : need to make sure that the other number after the operator is counted as the second operand 
+//   if(target.classList.contains('op')){
+//     if (target.id === 'add-btn'){
+//       operator = '+' 
+//       value = value + operator
+//       display.innerHTML = value
+
+//     } else if ( target.id === 'division-btn'){
+//       operator.replace(operator, '/') 
+//       value = value + operator
+//       display.innerHTML = value
+//     console.log(display.innerHTML)
+//   }} 
+
+//   //DECIMAL PLACE
+//   if(target.id === 'decimal-btn'){
+//     value = value + '.'
+//     display.innerHTML = value
+
+//   }
+//   })
 
 
